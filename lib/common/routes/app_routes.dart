a0f7ef2591c183b5655cf4b7815ca95bab82f0c9
Awesomewsdm <awesomewsdm@gmail.com>
2024-01-04 07:50:16 +0000

    Remove all routes from AppRouter

--- lib/common/routes/app_routes.dart
+++ lib/common/routes/app_routes.dart
@@ -3,43 +3,5 @@ import "package:auto_route/auto_route.dart";
 @AutoRouterConfig(replaceInRouteName: "Screen,Route")
 class AppRouter extends $AppRouter {
   @override
-  List<AutoRoute> get routes => [
-        AutoRoute(
-          page: SignInRoute.page,
-          initial: true,
-        ),
-        AutoRoute(
-          page: SignUpRoute.page,
-        ),
-        AutoRoute(
-          page: PasswordResetRoute.page,
-        ),
-        AutoRoute(
-          page: ProfileRoute.page,
-        ),
-        AutoRoute(
-          page: ChangePasswordRoute.page,
-        ),
-        AutoRoute(
-          page: LoadingRoute.page,
-        ),
-        AutoRoute(
-          page: EditProfileRoute.page,
-        ),
-        AutoRoute(
-          page: SecurityCheckRoute.page,
-        ),
-        AutoRoute(
-          page: SetNewEmailRoute.page,
-        ),
-        AutoRoute(
-          page: EnterEmailRoute.page,
-        ),
-        AutoRoute(
-          page: VerifyAccessRoute.page,
-        ),
-        AutoRoute(
-          page: SetNewPasswordRoute.page,
-        ),
-      ];
+  List<AutoRoute> get routes => [];
 }
