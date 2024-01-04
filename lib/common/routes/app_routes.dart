82bd87c57fbc93e7219e1ec6bea57bdc2c9c1266
Awesomewsdm <awesomewsdm@gmail.com>
2024-01-04 17:18:41 +0000

    Update imports in app_bloc.dart and add AutoRouteGuard in app_routes.dart

--- lib/common/routes/app_routes.dart
+++ lib/common/routes/app_routes.dart
@@ -2,7 +2,16 @@ import "package:auto_route/auto_route.dart";
 import "package:home_service_app/common/routes/app_routes.gr.dart";
 
 @AutoRouterConfig(replaceInRouteName: "Screen,Route")
-class AppRouter extends $AppRouter {
+class AppRouter extends $AppRouter implements AutoRouteGuard {
+  @override
+  void onNavigation(NavigationResolver resolver, StackRouter router) {
+    // if(isAuthenticated || resolver.route.name == LoginRoute.name){
+
+    //     resolver.next();
+    // }else{
+    //       resolver.redirect(LoginRoute(onResult:(didLogin)=> resolver.next(didLogin)))
+    // }
+  }
   @override
   List<AutoRoute> get routes => [
         AutoRoute(page: HomeRoute.page),
