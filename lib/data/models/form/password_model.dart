c1bc771efe967423953117d5a1d1906d1f06c1b6
Awesomewsdm <awesomewsdm@gmail.com>
2024-01-10 08:16:31 +0000

    Add password validation error messages

--- lib/data/models/form/password_model.dart
+++ lib/data/models/form/password_model.dart
@@ -2,7 +2,17 @@ import "package:formz/formz.dart";
 
 /// Validation errors for the [Password] [FormzInput].
 enum PasswordValidationError {
-  invalid,
+  invalid("Invalid password"),
+  empty("Password is empty"),
+  noUpperCase("Password does not contain an uppercase letter"),
+  noLowerCase("Password does not contain a lowercase letter"),
+  noNumber("Password does not contain a number"),
+  noSpecialCharacter("Password does not contain a special character"),
+  isNotLongEnough("Password should be at least 8 characters long");
+
+  /// Generic invalid error.
+  const PasswordValidationError(this.message);
+  final String message;
 }
 
 class Password extends FormzInput<String, PasswordValidationError> {
@@ -14,11 +24,24 @@ class Password extends FormzInput<String, PasswordValidationError> {
 
   @override
   PasswordValidationError? validator(String? value) {
-    if (value == null || value.isEmpty) {
+    if (value!.isEmpty) {
       return PasswordValidationError.empty;
-    }
-    if (!_passwordRegExp.hasMatch(value)) {
+    } else if (!_passwordRegExp.hasMatch(value)) {
       return PasswordValidationError.invalid;
+    } else if (!value.contains(RegExp("[A-Z]"))) {
+      return PasswordValidationError.noUpperCase;
+    } else if (!value.contains(RegExp("[a-z]"))) {
+      return PasswordValidationError.noLowerCase;
+    } else if (!value.contains(RegExp("[0-9]"))) {
+      return PasswordValidationError.noNumber;
+    } else if (!value.contains(
+      RegExp(
+        r'[!@#$%^&*(),.?":{}|<>]',
+      ),
+    )) {
+      return PasswordValidationError.noSpecialCharacter;
+    } else if (value.length < 8) {
+      return PasswordValidationError.isNotLongEnough;
     }
     return null;
   }
