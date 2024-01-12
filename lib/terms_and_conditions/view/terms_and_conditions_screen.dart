70c4293eb2b1dab811d5ef56055395a27c4588e1
Awesomewsdm <awesomewsdm@gmail.com>
2024-01-12 13:42:46 +0000

    added terms and conditions

---
+++ lib/terms_and_conditions/view/terms_and_conditions_screen.dart
@@ -0,0 +1,16 @@
+import "package:flutter/material.dart";
+
+class TermasAndConditions extends StatelessWidget {
+  const TermasAndConditions({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("TermasAndConditions"),
+      ),
+      body: const Center(
+        child: Text("TermasAndConditions"),
+      ),
+    );
+  }
+}
