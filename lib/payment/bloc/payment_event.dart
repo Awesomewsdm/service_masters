17b37dbcaa331e78e9502d7c842fb343657e6919
Awesomewsdm <awesomewsdm@gmail.com>
2024-01-21 23:46:45 +0000

    Update user authentication logic

---
+++ lib/payment/bloc/payment_event.dart
@@ -0,0 +1,8 @@
+import "package:service_masters/common/barrels.dart";
+
+abstract class PaymentEvent extends Equatable {
+  const PaymentEvent();
+
+  @override
+  List<Object> get props => [];
+}
---
+++ lib/payment/bloc/payment_state.dart
@@ -0,0 +1,8 @@
+import "package:service_masters/common/barrels.dart";
+
+abstract class PaymentState extends Equatable {
+  const PaymentState();
+
+  @override
+  List<Object> get props => [];
+}
