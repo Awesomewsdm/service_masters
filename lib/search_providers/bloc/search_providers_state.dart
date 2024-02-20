32d9e301eaf2c684491e4b218445e4c213708c34
Awesomewsdm <awesomewsdm@gmail.com>
2024-02-20 16:25:06 +0000

    b

---
+++ lib/search_providers/bloc/search_providers_state.dart
@@ -0,0 +1,7 @@
+// part of 'search_providers_bloc.dart';
+
+// @freezed
+// class SearchProvidersState with _$SearchProvidersState {
+//   const factory SearchProvidersState.initial() = _Initial;
+//   const factory SearchProvidersState.loadInProgress() = _LoadInProgress;
+// }
