9537e2d5a759833d8e54318be3f4e97d5540a636
Awesomewsdm <awesomewsdm@gmail.com>
2024-03-02 18:57:06 +0000

    Refactor code to improve performance and readability

--- lib/data/models/service_provider/service_provider.model.dart
+++ lib/data/models/service_provider/service_provider.model.dart
@@ -1,3 +1,4 @@
+import "package:json_annotation/json_annotation.dart";
 import "package:service_masters/common/barrels.dart";
 
 part "service_provider.model.g.dart";
@@ -10,7 +11,12 @@ class ServiceProvider with _$ServiceProvider {
     required double rating,
     required String location,
     required bool isVerified,
-    @JsonKey(name: "created_at") required DateTime createdAt,
+    @JsonKey(
+      name: "created_at",
+      fromJson: _timestampFromJson,
+      toJson: _timestampToJson,
+    )
+    required Timestamp createdAt,
     @JsonKey(name: "first_name") required String firstName,
     @JsonKey(name: "last_name") required String lastName,
     @JsonKey(name: "years_of_experience") required String yearsOfExperience,
@@ -25,3 +31,8 @@ class ServiceProvider with _$ServiceProvider {
   factory ServiceProvider.fromJson(Map<String, dynamic> json) =>
       _$ServiceProviderFromJson(json);
 }
+
+Timestamp _timestampFromJson(int json) =>
+    Timestamp.fromMillisecondsSinceEpoch(json);
+
+int _timestampToJson(Timestamp timestamp) => timestamp.millisecondsSinceEpoch;
