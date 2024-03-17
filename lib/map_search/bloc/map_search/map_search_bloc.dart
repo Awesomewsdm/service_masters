cac774e2058149e75fbdfe47316f291d03deb163
Awesomewsdm <awesomewsdm@gmail.com>
2024-03-17 00:10:24 +0000

    Commented out unused imports and code

--- lib/map_search/bloc/map_search/map_search_bloc.dart
+++ lib/map_search/bloc/map_search/map_search_bloc.dart
@@ -1,12 +1,12 @@
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:freezed_annotation/freezed_annotation.dart';
+// import 'package:flutter_bloc/flutter_bloc.dart';
+// import 'package:freezed_annotation/freezed_annotation.dart';
 
-part 'map_search_state.dart';
-part 'map_search_event.dart';
-part 'map_search_bloc.freezed.dart';
+// part 'map_search_state.dart';
+// part 'map_search_event.dart';
+// part 'map_search_bloc.freezed.dart';
 
-class MapSearchBloc extends Bloc<MapSearchEvent,MapSearchState>{
-  MapSearchBloc() : super(const MapSearchState.initial()){
+// class MapSearchBloc extends Bloc<MapSearchEvent,MapSearchState>{
+//   MapSearchBloc() : super(const MapSearchState.initial()){
     
-  }
-}
+//   }
+// // }
