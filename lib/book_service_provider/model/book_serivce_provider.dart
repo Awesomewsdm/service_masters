2b26aca1e68f26a91ae51e35e53719f9660dac44
Awesomewsdm <awesomewsdm@gmail.com>
2024-04-27 14:49:41 +0000

    Refactor BookServiceProvider model and AppRoutes file

--- lib/book_service_provider/model/book_serivce_provider.dart
+++ lib/book_service_provider/model/book_serivce_provider.dart
@@ -1,1 +1,48 @@
+import "package:service_masters/common/barrels.dart";
 
+part ""
+
+@JsonSerializable()
+class BookServiceProvider {
+  BookServiceProvider({
+    required this.id,
+    required this.customerId,
+    required this.serviceProviderId,
+    required this.serviceId,
+    required this.bookingDate,
+    required this.bookingTime,
+    required this.status,
+    required this.description,
+    this.mediaFilesUrl,
+  });
+
+  factory BookServiceProvider.fromJson(Map<String, dynamic> json) =>
+      _$BookServiceProviderFromJson(json);
+
+  final String id;
+  final String customerId;
+  final String serviceProviderId;
+  final String serviceId;
+  final DateTime bookingDate;
+  final String bookingTime;
+  final String status;
+  final String description;
+  final List<String>? mediaFilesUrl;
+
+  String get statusText {
+    switch (status) {
+      case "0":
+        return "Pending";
+      case "1":
+        return "Accepted";
+      case "2":
+        return "Rejected";
+      case "3":
+        return "Completed";
+      default:
+        return "Pending";
+    }
+  }
+
+  Map<String, dynamic> toJson() => _$BookServiceProviderToJson(this);
+}
