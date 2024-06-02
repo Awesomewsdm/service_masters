b95f27824d736f736b990d523a9d56017c3aaab7
Awesomewsdm <awesomewsdm@gmail.com>
2024-06-02 10:17:34 +0000

    feat: Add serviceProviders list to ChatState

--- lib/chat/bloc/chat_state.dart
+++ lib/chat/bloc/chat_state.dart
@@ -5,6 +5,7 @@ class ChatState with _$ChatState {
   const factory ChatState({
     @Default([]) List<Chat> chats,
     @Default([]) List<Message> messages,
+    @Default([]) List<ServiceProvider> serviceProviders,
     @Default("") String errorMessage,
     @Default(ChatStatus.initial) ChatStatus initial,
   }) = _ChatState;
